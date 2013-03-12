271b3056b7fad176f230591bfdc4f98866225a8f
Adrian R.M <adrian@ldx9.com>
2013-03-12 17:22:57 -0600

    Rails Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ldx9::Application.config.secret_token = 'c060247223fdcd9ad8d94413db3830174a6fe43fab77dc7cb3593786c06799da25a970e8ba0be17675afccff00ec9f1d6265d236597724364a296eb78a55dd01'
